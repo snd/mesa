4ca767d7400bcf86ecb41fc0d21e208a58607425
Maximilian Krüger <kruemaxi@googlemail.com>
2014-12-17 17:30:12 +0100

    improve schema

--- test/schema.sql
+++ test/schema.sql
@@ -1,4 +1,22 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE "user"(
-  id SERIAL NOT NULL,
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
   name TEXT NOT NULL
+);
+
+INSERT INTO "user"(name) VALUES
+  ('laura'),
+  ('dale'),
+  ('audrey'),
+  ('leland'),
+  ('donna'),
+  ('ben')
+;
+
+CREATE TABLE event(
+  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_id uuid NOT NULL REFERENCES "user"(id),
+  created_at timestamptz NOT NULL,
+  data JSON
 );
